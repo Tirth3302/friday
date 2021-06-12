68b83c64355cf8bfafc71477e0fca0f667eec276
Tirth <tirthpatel3302@gmail.com>
2021-06-12 10:03:27 +0530

    Second Commit

---
+++ friday.py
@@ -0,0 +1,116 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[0].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning sir")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon sir")
+
+    else:
+        speak("Good Evening sir")
+
+    speak("I am Friday. Please tell me how may I help you?")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en=in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        #print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+'''def sendEmail(to, content):
+    #have to allow less secure apps
+    server = smtplib.SMTP('smntp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password-here')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()'''
+
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()    
+    
+    #logic for executing task based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 1)
+            #sentences = 1 means it will read 1 sentence from wikipedia
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            music_dir = 'H:\TIRTH COMPUTER\YOU TUBE\Background music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+            #song[0] means will always play 1st music
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Program Files\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        '''
+        elif 'email to tirth' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "tirthpatel3302@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir, I am not able to send this email")
+        '''
+
